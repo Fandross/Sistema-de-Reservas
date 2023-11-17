98bf583c725350ebd80040d4ee7514f0f6e2a08e
Fandross <1petterwinner@gmail.com>
2023-11-17 09:09:19 -0300

    Criação base do Projeto
    Criação base do Projeto com fogo no app.py e app.js, configurando o sistema para poder cadastrar pessoas aos eventos e ao administrador poder excluir

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,83 @@
-from flask import Flask
+from flask import Flask, jsonify, request, session
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
+app.secret_key = 'your_secret_key'
+
+eventos = [
+    {"id": 1, "nome": "Rodizio de Pizzas por R$19,90", "data": "01-12-2023", "horario": "18:00", "capacidade": 50},
+    {"id": 2, "nome": "Rodizio de Massas por R$29,90", "data": "06-12-2023", "horario": "19:30", "capacidade": 30},
+]
+
+reservas = []
+email_acesso = None  # Adicione esta linha
 
 @app.route('/')
 def hello():
-    return 'Backend do Sistema de Reservas'
+    return 'Backend do Sistema de Reservas Ativado'
+
+@app.route('/eventos', methods=['GET'])
+def listar_eventos():
+    return jsonify(eventos)
+
+# Registrar Evento
+@app.route('/eventos/<int:evento_id>/registrar', methods=['POST'])
+def registrar_evento(evento_id):
+    global reservas, email_acesso
+
+    evento = next((e for e in eventos if e['id'] == evento_id), None)
+    if evento is None:
+        return jsonify({"mensagem": "Evento não encontrado"}), 404
+
+    # Obter o nome do usuário logado (você pode usar a variável de sessão ou outra forma de autenticação)
+    email_usuario = request.json.get('emailUsuario')  # Ajuste conforme necessário
+
+    # Verificar se o usuário já está registrado para este evento
+    if any(reserva['evento']['id'] == evento_id and reserva['emailUsuario'] == email_usuario for reserva in reservas):
+        return jsonify({"mensagem": "Você já está registrado para este evento."}), 400
+
+    # Se o usuário não estiver registrado, atualize o email_acesso
+    if email_acesso is None:
+        email_acesso = email_usuario
+
+    # Verificar se o email do usuário é o mesmo usado para acessar o site
+    if email_usuario != email_acesso:
+        return jsonify({"mensagem": "Você só pode se registrar com o mesmo email usado para acessar o site."}), 400
+
+    reservas.append({"evento": evento, "emailUsuario": email_usuario})
+    return jsonify({"mensagem": "Registro feito com sucesso!"})
+# Detalhes do Evento
+@app.route('/eventos/<int:evento_id>/detalhes', methods=['GET'])
+def detalhes_evento(evento_id):
+    evento = next((e for e in eventos if e['id'] == evento_id), None)
+    if evento is None:
+        return jsonify({"mensagem": "Evento não encontrado"}), 404
+
+    detalhes = {"evento": evento, "reservas": []}
+
+    for reserva in reservas:
+        if reserva['evento']['id'] == evento_id:
+            detalhes['reservas'].append({"emailUsuario": reserva['emailUsuario']})
+
+    return jsonify(detalhes)
+
+# Excluir Reserva
+@app.route('/eventos/<int:evento_id>/excluir-reserva/<email_reserva>', methods=['DELETE'])
+def excluir_reserva(evento_id, email_reserva):
+    global reservas
+
+    evento = next((e for e in eventos if e['id'] == evento_id), None)
+    if evento is None:
+        return jsonify({"mensagem": "Evento não encontrado"}), 404
+
+    reservas_evento = [reserva for reserva in reservas if reserva['evento']['id'] == evento_id and reserva['emailUsuario'] == email_reserva]
+
+    if not reservas_evento:
+        return jsonify({"mensagem": "Reserva não encontrada para exclusão"}), 404
+
+    reservas.remove(reservas_evento[0])
+    return jsonify({"mensagem": "Reserva excluída com sucesso!"})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Frontend/sistema-de-reservas/src/App.js
+++ Frontend/sistema-de-reservas/src/App.js
@@ -1,24 +1,153 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
+  const [backendMessage, setBackendMessage] = useState('');
+  const [eventos, setEventos] = useState([]);
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [detalhesEvento, setDetalhesEvento] = useState(null);
+  const [nomeUsuario, setNomeUsuario] = useState('');
+  const [registroAberto, setRegistroAberto] = useState(false);
+
+  useEffect(() => {
+    const nomeUsuario = window.prompt('Digite seu email para acessar:');
+    setNomeUsuario(nomeUsuario);
+    setIsAdmin(nomeUsuario === 'admin');
+
+    fetch('http://127.0.0.1:5000/', { credentials: 'include' })
+      .then(response => response.text())
+      .then(message => setBackendMessage(message))
+      .catch(error => console.error('Erro ao obter dados do backend:', error));
+
+    fetch('http://127.0.0.1:5000/eventos', { credentials: 'include' })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Lista de Eventos:', data);
+        setEventos(data);
+      })
+      .catch(error => console.error('Erro ao obter lista de eventos:', error));
+  }, []);
+
+  const handleDetalhesEvento = (eventoId) => {
+    fetch(`http://127.0.0.1:5000/eventos/${eventoId}/detalhes`)
+      .then(response => response.json())
+      .then(data => setDetalhesEvento(data))
+      .catch(error => console.error('Erro ao obter detalhes do evento:', error));
+  };
+
+  const handleRegistrarEvento = (eventoId, nomeUsuario) => {
+    // Pedir ao usuário para confirmar o email
+    const emailConfirmacao = window.prompt('Digite seu email novamente para confirmar o registro:');
+
+    // Log para verificar se o email está correto
+    console.log('Email de confirmação:', emailConfirmacao);
+
+    // Verificar se o email de confirmação é o mesmo
+    if (emailConfirmacao !== nomeUsuario) {
+      alert('Os emails não coincidem. Tente novamente.');
+      return;
+    }
+
+    // Continuar com o registro
+    fetch(`http://127.0.0.1:5000/eventos/${eventoId}/registrar`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        emailUsuario: nomeUsuario,
+      }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        alert(data.mensagem);
+        fetch('http://127.0.0.1:5000/eventos')
+          .then(response => response.json())
+          .then(data => {
+            console.log('Lista de Eventos:', data);
+            setEventos(data);
+          })
+          .catch(error => console.error('Erro ao obter lista de eventos:', error));
+      })
+      .catch(error => console.error('Erro ao registrar no evento:', error));
+  };
+
+  const handleExcluirReserva = (eventoId, nomeReserva) => {
+    fetch(`http://127.0.0.1:5000/eventos/${eventoId}/excluir-reserva/${encodeURIComponent(nomeReserva)}`, {
+      method: 'DELETE',
+    })
+      .then(response => response.json())
+      .then(data => {
+        alert(data.mensagem);
+        handleDetalhesEvento(eventoId);
+      })
+      .catch(error => console.error('Erro ao excluir reserva:', error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={(
+          <div>
+            {isAdmin && (
+              <div>
+                <h2>Mensagem do Backend:</h2> <p>{backendMessage}</p>
+              </div>
+            )}
+            <header>
+              <h1>Espaco das Massas UNP</h1>
+            </header>
+            <h2>Lista de Eventos:</h2>
+            <ul>
+              {eventos.map(evento => (
+                <li key={evento.id}>
+                  {evento.nome} - {evento.data} às {evento.horario} ({evento.capacidade} vagas)
+                  <button onClick={() => handleDetalhesEvento(evento.id)}>Ver Detalhes</button>
+                  {!isAdmin && (
+                    <button onClick={() => {
+                      handleRegistrarEvento(evento.id, nomeUsuario);
+                      setRegistroAberto(false);
+                    }}>Registrar</button>
+                  )}
+                </li>
+              ))}
+            </ul>
+
+            {detalhesEvento && (
+              <div>
+                <h2>Detalhes do Evento:</h2>
+                <p>Nome: {detalhesEvento.evento.nome}</p>
+                <p>Data: {detalhesEvento.evento.data}</p>
+                <p>Horário: {detalhesEvento.evento.horario}</p>
+                <p>Capacidade: {detalhesEvento.evento.capacidade}</p>
+
+                <h3>Reservas:</h3>
+                <ul>
+                  {detalhesEvento.reservas.map(reserva => (
+                    <li key={reserva.emailUsuario}>
+                      {reserva.emailUsuario}{' '}
+                      {isAdmin && (
+                        <>
+                          <button onClick={() => handleExcluirReserva(detalhesEvento.evento.id, reserva.emailUsuario)}>Excluir</button>{' '}
+                        </>
+                      )}
+                      {registroAberto && !isAdmin && (
+                        <button onClick={() => {
+                          handleRegistrarEvento(detalhesEvento.evento.id, nomeUsuario);
+                          setRegistroAberto(false);
+                        }}>Registrar</button>
+                      )}
+                    </li>
+                  ))}
+                </ul>
+
+              </div>
+            )}
+          </div>
+        )} />
+        {/*... outras rotas */}
+      </Routes>
+    </Router>
   );
 }
 
