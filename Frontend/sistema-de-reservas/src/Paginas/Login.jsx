547c31127d7dd6b5f7b82bb70b45d7c4953e1d00
Fandross <1petterwinner@gmail.com>
2023-11-29 21:39:01 -0300

    Update login
    Marcio Felipe refatorou o codigo
    
    Co-Authored-By: Necxus1 <59101030+necxus1@users.noreply.github.com>

--- Frontend/sistema-de-reservas/src/Paginas/Login.jsx
+++ Frontend/sistema-de-reservas/src/Paginas/Login.jsx
@@ -20,14 +20,11 @@ const Login = () => {
       const data = await response.json();
   
       if (response.ok) {
-        // Autenticação bem-sucedida, atualize o nome do usuário e armazene o token
         console.log('Token recebido:', data.token);
-        // onLogin(email);
         localStorage.setItem('token', data.token);
         console.log('Logado!')
         navigate('/');
       } else {
-        // Exiba uma mensagem de erro
         alert(data.mensagem);
       }
     } catch (error) {
@@ -44,12 +41,12 @@ const Login = () => {
           <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div className="mb-6">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Password:</label>
+          <label className="block text-gray-700 text-sm font-bold mb-2">Senha:</label>
           <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </div>
         <div className="flex items-center justify-between">
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={handleLogin}>
-            Login
+            Logar
           </button>
         </div>
       </form>
